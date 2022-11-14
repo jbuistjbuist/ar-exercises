9383f5188cb68808027776a67c8a73adb126dec9
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2022-11-14 18:11:31 -0500

    Complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,12 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+#adding employees associated with the different stores. knows they are associated becuase of modifying the class definitions
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Goolash", last_name: "Pumpkin", hourly_rate: 20)
+@store1.employees.create(first_name: "Hello", last_name: "There", hourly_rate: 15)
+
+@store2.employees.create(first_name: "Yum", last_name: "Food", hourly_rate: 100)
+@store2.employees.create(first_name: "Cry", last_name: "Alone", hourly_rate: 10)
+@store2.employees.create(first_name: "Sing", last_name: "Anthem", hourly_rate: 25)
