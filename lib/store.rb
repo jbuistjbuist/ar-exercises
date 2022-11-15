83c3a1330127c7b279ce91f8f56671ae24ebefa1
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2022-11-14 20:00:05 -0500

    Complete exercise 9 (stretch)

--- lib/store.rb
+++ lib/store.rb
@@ -6,10 +6,22 @@ class Store < ActiveRecord::Base
   validates :name, length: {minimum: 3}
   validate :need_to_sell_something
 
+  before_destroy :permission_to_destroy?, prepend: true
+
+  private
+
   def need_to_sell_something
     if !womens_apparel && !mens_apparel
       errors.add("Validation error:", "must sell either women's or men's apparel")
     end
   end
+
+  def permission_to_destroy?
+    if self.employees.length() > 0
+      self.errors.add(:base, "Cannot destroy a store with employees")
+      throw :abort
+    end
+  end
+
 
 end
